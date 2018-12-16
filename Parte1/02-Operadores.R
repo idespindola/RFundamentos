4951fd71327ab90e83b29ce3167337464c1048e7
Igor Espindola <igorespindola@id.uff.br>
2018-12-16 14:51:01 -0200

    Segunda aula

---
+++ Parte1/02-Operadores.R
@@ -0,0 +1,46 @@
+## Operadores Básicos, Relacionais e Lógicos em R
+
+# Operadores Básicos
+
+# Soma
+4 + 4
+
+# Subtração
+4 - 4
+
+# Multiplicação
+4 * 4
+
+# Divisão
+4 / 4
+
+# Potência
+4^2
+4**2
+
+# Módulo
+14 %% 3
+
+# Operadores Relacionais
+
+# Atribuindo variáveis
+
+a = 7
+b = 5
+
+# Operadores
+a > 8
+a < 8
+a <= 8
+a >= 8
+a == 8
+a != 8
+
+
+# Operadores Lógicos
+
+(a==8) & (b==6)
+(a==8) | (b>5)
+
+a > 8
+print(!a > 8)
